32fb858d972983dc5b4f3946b3e45937d41525f6
pedrobartolini <pedrobartolini@outlook.com>
2024-06-03 18:15:14 -0300

    first release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rust ASCII image printer
+
+### Description:
+- `--image` - path to the image
+- `--w` - width of the output image
+- `--h` - height of the output image
+
+
+### Example:
+`ascii-image-printer --image ./image.jpg --w 100 --h 100`
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ const CHARS: [u8; CHARS_LEN] = [
    b'$', b'@', b'B', b'%', b'8', b'&', b'W', b'M', b'#', b'*', b'o', b'a', b'h', b'k', b'b', b'd', b'p', b'q', b'w', b'm', b'Z', b'O', b'0', b'Q', b'L', b'C', b'J', b'U', b'Y', b'X', b'z', b'c', b'v', b'u', b'n', b'x', b'r', b'j', b'f', b't', b'/', b'\\', b'|', b'(', b')', b'1', b'{', b'}', b'[', b']', b'?', b'-', b'_', b'+', b'~', b'<', b'>', b'i', b'!', b'l', b'I', b';', b':', b',', b'"', b'^', b'`', b'.'
 ];
 
-const MAX_CANVAS_WIDTH: u32 = 40;
-const MAX_CANVAS_HEIGHT: u32 = 20;
+// const MAX_CANVAS_WIDTH: u32 = 40;
+// const MAX_CANVAS_HEIGHT: u32 = 20;
 
 fn get_ascii_with_grayscale_value(grayscale_value: u8) -> char {
    let ascii_index = ((CHARS_LEN as f64) / 255.0) * grayscale_value as f64;
@@ -18,37 +18,47 @@ fn get_ascii_with_grayscale_value(grayscale_value: u8) -> char {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
    // --image <image_path>
+   // --w <width>
+   // --h <height>
    let args: Vec<String> = std::env::args().collect();
 
-   println!("{:?}", args);
+   let mut image_path = "";
+   let mut max_width: u32 = 0;
+   let mut max_height: u32 = 0;
 
-   if args.len() != 3 {
-      eprintln!("Usage: --image <image_path>");
-      return Err("Invalid arguments 1".into());
+   for i in 0..args.len() {
+      if args[i] == "--image" {
+         image_path = &args[i + 1];
+      } else if args[i] == "--w" {
+         max_width = args[i + 1].parse::<u32>().unwrap();
+      } else if args[i] == "--h" {
+         max_height = args[i + 1].parse::<u32>().unwrap();
+      }
    }
 
-   if args[1] != "--image" {
-      eprintln!("Usage: --image <image_path>");
-      return Err("Invalid arguments 2".into());
+   if image_path.is_empty() {
+      return Err("Please provide an image path".into());
    }
 
-   let image = &args[2];
+   if max_width == 0 || max_height == 0 {
+      return Err("Please provide width and height".into());
+   }
 
-   let target_image = image::open(image)?.grayscale();
+   let target_image = image::open(image_path)?.grayscale();
 
    let (width, height) = target_image.dimensions();
    let aspect_ratio = width as f64 / height as f64;
 
-   let new_width = width / MAX_CANVAS_WIDTH * MAX_CANVAS_WIDTH;
+   let new_width = width / max_width * max_width;
    let new_height = (new_width as f64 / aspect_ratio) as u32;
 
    let target_image = target_image.resize_exact(new_width, new_height as u32, image::imageops::FilterType::Nearest);
 
-   let width_batch_size = new_width / MAX_CANVAS_WIDTH;
-   let height_batch_size = new_height / MAX_CANVAS_HEIGHT;
+   let width_batch_size = new_width / max_width;
+   let height_batch_size = new_height / max_height;
 
-   for y in 0..MAX_CANVAS_HEIGHT {
-      for x in 0..MAX_CANVAS_WIDTH {
+   for y in 0..max_height {
+      for x in 0..max_width {
          let mut grayscale_value: u32 = 0;
 
          for j in 0..height_batch_size {
